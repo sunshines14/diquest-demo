c83130ed5cf17a98e136cf56c0030f971be4c2a1
sunshines14 <sunshines14@github.com>
2021-07-12 01:53:24 +0900

    warning ignore

--- run_demo.sh
+++ run_demo.sh
@@ -9,10 +9,10 @@ if [ $stage -le 0 ]; then
 fi
 
 if [ $stage -le 1 ]; then
-    python3 $basedir/aed-preprob.py $basedir/input/input-16k.wav $basedir/aed-files/ || exit 1;
+    python3 -W ignore $basedir/aed-preprob.py $basedir/input/input-16k.wav $basedir/aed-files/ || exit 1;
     find $basedir/aed-files -name '*.wav' | sort > $basedir/input/aed-files.txt || exit 1;
     
-    python3 $basedir/aed.py $basedir/model/11model-194-0.6464-0.8788.tflite $basedir/input/aed-files.txt $basedir/output/aed-result.csv || exit 1;
+    python3 -W ignore $basedir/aed.py $basedir/model/11model-194-0.6464-0.8788.tflite $basedir/input/aed-files.txt $basedir/output/aed-result.csv || exit 1;
 fi
 
 if [ $stage -le 2 ]; then
@@ -21,10 +21,10 @@ if [ $stage -le 2 ]; then
 fi
 
 if [ $stage -le 3 ]; then
-    python3 $basedir/asc-preprob.py $basedir/input/input-48k.wav $basedir/asc-files/ || exit 1;
+    python3 -W ignore $basedir/asc-preprob.py $basedir/input/input-48k.wav $basedir/asc-files/ || exit 1;
     find $basedir/asc-files -name '*.wav' | sort > $basedir/input/asc-files.txt || exit 1;
     
-    python3 $basedir/asc.py $basedir/model/3model-191-0.3737-0.9384.tflite $basedir/input/asc-files.txt $basedir/output/asc-result.csv || exit 1;
+    python3 -W ignore $basedir/asc.py $basedir/model/3model-191-0.3737-0.9384.tflite $basedir/input/asc-files.txt $basedir/output/asc-result.csv || exit 1;
 fi
 
 rm input/input.wav input/input-16k.wav input/input-48k.wav input/aed-files.txt input/asc-files.txt
